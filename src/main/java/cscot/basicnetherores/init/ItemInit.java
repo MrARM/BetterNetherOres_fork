06adda0495f6cd470b01270c0f979d9f33c2eed4
MrARM <jordan@thesmokinggnu.us>
2020-01-16 17:43:53 -0600

    Add files to git

---
+++ src/main/java/cscot/basicnetherores/init/ItemInit.java
@@ -0,0 +1,13 @@
+package cscot.basicnetherores.init;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import net.minecraft.item.Item;
+
+public class ItemInit {
+
+	public static final List<Item> ITEMS = new ArrayList<Item>();
+
+}
